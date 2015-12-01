67abb5177d5211dfa5966b40f709f761fdf541f6
Deepak Jois <deepak.jois@gmail.com>
2015-12-01 14:16:59 +0530

    Adding harfbuzz version

--- test/harfbuzz_test.lua
+++ test/harfbuzz_test.lua
@@ -1,6 +1,8 @@
 harfbuzz = require('luaharfbuzz')
 pl = require('pl.pretty')
 
+print("Using harfbuzz version", harfbuzz.version())
+
 local fontfile = io.open(arg[1], "r")
 local fontdata = fontfile:read("*all")
 fontfile:close()
