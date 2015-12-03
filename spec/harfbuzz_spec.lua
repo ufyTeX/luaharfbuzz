c8eef00477f24777ed3fb8acfc90d266e7f124b2
Deepak Jois <deepak.jois@gmail.com>
2015-12-03 12:51:02 +0530

    Added testing with busted

---
+++ spec/harfbuzz_spec.lua
@@ -0,0 +1,12 @@
+describe("harfbuzz module", function()
+  it("loads successfully", function()
+    local harfbuzz = require('harfbuzz')
+  end)
+end)
+
+describe("luaharfbuzz module", function()
+  it("loads successfully", function()
+    local harfbuzz = require('luaharfbuzz')
+  end)
+end)
+
