b1a64190ccb5dbb0943c86ee6175d5d4cbf4efd6
Deepak Jois <deepak.jois@gmail.com>
2015-12-05 07:05:29 +0530

    Moving API status info to a separate directory

--- scripts/generate_harfbuzz_api_list.sh
+++ scripts/generate_harfbuzz_api_list.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-grep  -e "^hb_" /usr/local/include/harfbuzz/hb*.h  | grep "(" | sed -e 's/ .*$//' | awk -F: '{print $2}' | sort
+grep  -e "^hb_" /usr/local/include/harfbuzz/hb*.h  | grep "(" | sed -e 's/ .*$//' | awk -F: '{print $2}' | sort > status/full_api.txt
