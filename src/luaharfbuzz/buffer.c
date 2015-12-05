7d7d20d666189870501895fdcead0e568da919c0
Deepak Jois <deepak.jois@gmail.com>
2015-12-05 16:49:20 +0530

    Adding language and script support.
    * hb_buffer_{get,set}_language
    * hb_buffer_{get,set}_script

--- src/luaharfbuzz/buffer.c
+++ src/luaharfbuzz/buffer.c
@@ -44,6 +44,54 @@ static int buffer_set_direction(lua_State *L) {
   return 0;
 }
 
+static int buffer_get_language(lua_State *L) {
+  Buffer *b = (Buffer *)luaL_checkudata(L, 1, "harfbuzz.Buffer");
+
+  lua_pushstring(L,hb_language_to_string(hb_buffer_get_language(*b)));
+
+  return 1;
+}
+
+static int buffer_set_language(lua_State *L) {
+  Buffer *b = (Buffer *)luaL_checkudata(L, 1, "harfbuzz.Buffer");
+  const char* l = luaL_checkstring(L, 2);
+  hb_language_t lang  = hb_language_from_string(l, strlen(l));
+
+  if (lang == HB_LANGUAGE_INVALID) {
+    lua_pushstring(L, "Invalid language");
+    lua_error(L);
+  }
+
+  hb_buffer_set_language(*b, lang);
+
+  return 0;
+}
+
+static int buffer_get_script(lua_State *L) {
+  Buffer *b = (Buffer *)luaL_checkudata(L, 1, "harfbuzz.Buffer");
+  char s[128];
+  hb_tag_to_string(hb_script_to_iso15924_tag(hb_buffer_get_script(*b)),s);
+  s[4] = '\0';
+  lua_pushstring(L,s);
+
+  return 1;
+}
+
+static int buffer_set_script(lua_State *L) {
+  Buffer *b = (Buffer *)luaL_checkudata(L, 1, "harfbuzz.Buffer");
+  const char* s = luaL_checkstring(L, 2);
+  hb_script_t script  = hb_script_from_string(s, strlen(s));
+
+  if (script == HB_SCRIPT_UNKNOWN) {
+    lua_pushstring(L, "Unknown script");
+    lua_error(L);
+  }
+
+  hb_buffer_set_script(*b, script);
+
+  return 0;
+}
+
 static int buffer_add_utf8(lua_State *L) {
   Buffer *b = (Buffer *)luaL_checkudata(L, 1, "harfbuzz.Buffer");
 
@@ -67,6 +115,10 @@ static const struct luaL_Reg buffer_methods[] = {
   { "add_utf8", buffer_add_utf8 },
   { "set_direction", buffer_set_direction },
   { "get_direction", buffer_get_direction },
+  { "set_language", buffer_set_language },
+  { "get_language", buffer_get_language },
+  { "set_script", buffer_set_script },
+  { "get_script", buffer_get_script },
   { "guess_segment_properties", buffer_guess_segment_properties },
   { NULL, NULL },
 };
